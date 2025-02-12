5d55bcf3cc3b849c8738912bad05d772986310d9
Dzsodie <drmakarazs@gmail.com>
2025-02-12 10:49:20 +0100

    update javadoc for repository

--- src/main/java/com/zetoinc/edf_file_service/repository/EdfMetadataRepository.java
+++ src/main/java/com/zetoinc/edf_file_service/repository/EdfMetadataRepository.java
@@ -11,6 +11,18 @@ import org.springframework.stereotype.Repository;
  * for the {@link EdfMetadata} entity. It allows interactions with the database
  * without requiring explicit SQL queries.
  * </p>
+ * <p>
+ * Since {@code JpaRepository} provides built-in methods such as {@code save()},
+ * {@code findById()}, {@code findAll()}, {@code deleteById()}, and more,
+ * there is no need to explicitly define them.
+ * </p>
+ *
+ * <p>
+ * Additional custom query methods can be defined as needed.
+ * </p>
+ *
+ * @author Zsuzsa Makara
+ * @see org.springframework.data.jpa.repository.JpaRepository
  */
 @Repository
 public interface EdfMetadataRepository extends JpaRepository<EdfMetadata, Long> {
